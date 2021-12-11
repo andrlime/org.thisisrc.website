ea6d7ec8c9f11f096a03bb0ead7b4a043d003d6f
Andrew Li <54134293+andrewleaf22@users.noreply.github.com>
2021-12-12 00:35:13 +0800

    new version of skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,122 @@
 import Head from 'next/head';
-import styles from '../styles/R.module.css';
+import styles from '../styles/Q.module.css';
 import React from 'react';
+import {isMobile} from 'react-device-detect';
+import ReactTypingEffect from 'react-typing-effect';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({menu: false, phone: isMobile});
+    this.menuHandler = this.menuHandler.bind(this);
+  }
+
+  menuHandler(e) {
+    let cState = !this.state.menu;
+    this.setState({menu: cState});
+  }
+
   render() {
     let nav = [
-      {name: "About us", link: "/register"},
+      {name: "About Us", link: "/register"},
       {name: "Register", link: "/register"},
-      {name: "Another link", link: "/register"}
+      {name: "Our Core Values", link: "/register"}
     ];
 
     let dates = [
-      {date: new Date('2021-12-01T00:00:00'), desc: "Date 1"},
-      {date: new Date('2022-01-01T00:00:00'), desc: "Date 2"},
-      {date: new Date('2022-02-01T00:00:00'), desc: "Date 3"}
+      {date: new Date('2021-12-01T00:00:00'), desc: "Date 1", longdesc: "long description goes here"},
+      {date: new Date('2022-01-01T00:00:00'), desc: "Date 2", longdesc: "long description goes here"},
+      {date: new Date('2022-02-01T00:00:00'), desc: "Date 3", longdesc: "long description goes here"}
     ];
 
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     //todo: make the timeline more interactive
 
-    return (
-      <>
-      <div className={styles.logo}>Logo will go here</div>
-      <div className={styles.menucont}>
-        <div className={styles.left}>
+    let menuWidth = this.state.phone && this.state.menu ? "100vw" : (this.state.menu ? "25vw" : "");
+    let words = ["Research", "Innovation", "Discovery"];
 
-          {nav.map((i,index) => (
-            <div className={styles.navitem}>{i.name}
-              <div className={styles.underline}></div>
-            </div>
+    return (
+      <div className={styles.earth}>
+      <div className={styles.sidebar}>
+
+        <div style={{width: menuWidth}} onClick={this.menuHandler} className={this.state.menu ? styles.menuactive : styles.menu}>
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+        </div>
+
+        <div className={styles.navlist} style={{display: this.state.menu ? "inline-block" : "none"}}>
+
+          {nav.map((item, i) => (
+            <div className={styles.underline}><a href={item.link}>{item.name}</a></div>
           ))}
 
         </div>
-       
-        <div className={styles.right}>
 
-            <p className={styles.datehead}>Dates and Deadlines</p>
-            {dates.map((i, index) => (
-              <p className={styles.datestr}><span>{months[i.date.getMonth()]} {i.date.getDate()}, {i.date.getFullYear()}</span> - {i.desc}</p>
-            ))}
-
-        </div>
       </div>
+      
+      <div className={styles.main}>
+        <div className={styles.nav}>
+          <img id={styles.logo} src="/this-logo.png"/>
+        </div>
 
-      <div className={styles.maincont}>
+        <div style={{backgroundImage: "url(/background-test.jpeg)", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"}} className={styles.chead}> 
+        <ReactTypingEffect text={words} cursorRenderer={cursor => <h1>{cursor}</h1>} eraseDelay="3000" speed="60" eraseSpeed="60" typingDelay="1000" cursor=" "
+        displayTextRenderer={(text, i) => {
+          return (
+            <p>
+              {text.split('').map((char, i) => {
+                const key = `${i}`;
+                return (
+                  <span
+                    key={key}
+                  >{char}</span>
+                );
+              })}
+            </p>
+          );
+        }}/></div>
 
-          <div className={styles.video}>
-            Video will go here
-          </div>
+        <div className={styles.mainContent}>
 
           <div className={styles.photos}>
-            <div className={styles.photo}>Photo 1. This will be a slideshow</div>
+            <img className={styles.photo} src="/background-test.jpeg"/>
+            <img className={styles.photo} src="/background-test.jpeg"/>
+            <img className={styles.photo} src="/background-test.jpeg"/>
+            <img className={styles.photo} src="/background-test.jpeg"/>
           </div>
 
-      </div>
+          <div className={styles.timeline}>
+            <div id={styles.box}>
 
-      <div className={styles.footer}>
-        <div className={styles.imagebox}>
-          <img src="/this-logo.png"/>
-        </div>
-        
-        <div className={styles.footerbox}>
+              {dates.map((item, i)=> (
+                <p><span>{months[item.date.getMonth()]} {item.date.getDate()}</span>{item.desc}<div className={styles.underline}></div><hr/>{item.longdesc}</p>
+              ))}
+              <p></p><p></p>
 
-            <p>Privacy Policy</p>
-            <p>Terms and Conditions</p>
-            <p>ICP Number</p>
-            <p>Designed by <span>Andrew Li THIS '22</span></p>
+            </div>
+            <div id={styles.line}></div>
+          </div>
 
         </div>
 
-      </div>
-      </>
+        <div className={styles.footer}>
+
+          <div className={styles.imgbox}><img src="logo-spartan.svg"/><img src="this-logo.png"/>
+          </div>
+
+          <div className={styles.text}><span>Privacy Policy</span>
+          <span>Terms and Conditions</span>
+          <span>Powered by React</span>
+          <span>Designed by <span>Andrew Li</span></span></div>
+
+          
+
+        </div>
+      </div></div>
     );
   }
 }
